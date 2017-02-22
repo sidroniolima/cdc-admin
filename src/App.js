a897d2a76e681ce82348a2eae432c4be014634e2
Sidronio Lima <sidronio@sidroniolima.com.br>
2017-02-22 17:29:01 -0300

    Eventos

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './css/side-menu.css';
 import './css/pure-min.css';
 import $ from 'jquery';
+import InputCustomizado from './componentes/InputCustomizado.js'
 
 class App extends Component {
 
@@ -9,7 +10,46 @@ class App extends Component {
   {
     super();
 
-    this.state = {lista : []};
+    this.state = {lista : [], nome: '', email: '', senha: ''};
+    
+    this.enviaForm = this.enviaForm.bind(this);
+
+    this.setNome = this.setNome.bind(this);
+    this.setEmail = this.setEmail.bind(this);
+    this.setSenha = this.setSenha.bind(this);
+  }
+
+  setNome(event)
+  {
+    this.setState({nome : event.target.value});
+  }
+
+  setEmail(event)
+  {
+   this.setState({email : event.target.value});
+  }
+
+  setSenha(event)
+  {
+    this.setState({senha : event.target.value});
+  }
+
+  enviaForm(event)
+  {
+    event.preventDefault();
+
+    $.ajax({
+      url: 'https://cdc-react.herokuapp.com/api/autores',
+      contentType: 'application/json',
+      dataType: 'json',
+      type: 'post',
+      data: JSON.stringify({nome : this.state.nome, email : this.state.email, senha : this.state.senha}),
+      success: function(resposta)
+      {
+        this.setState({lista : resposta});
+      },
+      error: (erro => console.log(erro))
+    })
   }
 
   componentDidMount()
@@ -56,21 +96,15 @@ class App extends Component {
             <div className="content" id="content">
               <div className="pure-form pure-form-aligned">
                 <form className="pure-form pure-form-aligned">
-                  <div className="pure-control-group">
-                    <label htmlFor="nome">Nome</label> 
-                    <input id="nome" type="text" name="nome" value=""  />                  
-                  </div>
-                  <div className="pure-control-group">
-                    <label htmlFor="email">Email</label> 
-                    <input id="email" type="email" name="email" value=""  />                  
-                  </div>
-                  <div className="pure-control-group">
-                    <label htmlFor="senha">Senha</label> 
-                    <input id="senha" type="password" name="senha"  />                                      
-                  </div>
+
+                  <InputCustomizado id="nome" label="Nome" type="text" value={this.state.nome} onChange={this.setNome}/>                  
+                  <InputCustomizado id="email" label="Email" type="email" value={this.state.email} onChange={this.setEmail}/>
+                  <InputCustomizado id="senha" label="Senha" type="secret" value={this.state.senha} onChange={this.setSenha}/>
+
+
                   <div className="pure-control-group">                                  
                     <label></label> 
-                    <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
+                    <button type="submit" className="pure-button pure-button-primary" onSubmit={this.enviaForm}>Gravar</button>
                   </div>
                 </form>             
 
